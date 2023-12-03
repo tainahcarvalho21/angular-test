364a1d3cda80cb3a5ca6422164656b8d7eac70ff
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-12-02 23:02:07 -0300

    initial settings

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -10,7 +11,8 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
