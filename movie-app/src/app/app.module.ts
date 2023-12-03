250bad1d5eb20982b67c23573c2f79401677433a
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-12-03 00:22:18 -0300

    I generated the components and created the basic structure of the navbar HTML

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FilmComponent } from './pages/film/film.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FilmComponent,
+    NavBarComponent,
+    FiltersComponent,
+    MovieListComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
--- movie-app/src/app/pages/film/film.component.spec.ts
+++ movie-app/src/app/pages/film/film.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { FilmComponent } from './film.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('FilmComponent', () => {
+  let component: FilmComponent;
+  let fixture: ComponentFixture<FilmComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [FilmComponent]
     });
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(FilmComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
